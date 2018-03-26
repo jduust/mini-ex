41beb4304e4423b7150914b5d7a57e7a93beb59b
Jakob Duus Terkelsen <36190025+jduust@users.noreply.github.com>
2018-03-26 14:02:37 +0200

    Add files via upload

---
+++ mini_ex7/README.md
@@ -0,0 +1,6 @@
+# Mini Exercise 7
+[![Screenshot](https://raw.githubusercontent.com/jduust/mini-ex/master/mini_ex4/screenshot.PNG)](https://rawgit.com/jduust/mini-ex/master/mini_ex7/index.html)
+
+[Try it out here](https://rawgit.com/jduust/mini-ex/master/mini_ex7/index.html)
+
+## The program
---
+++ mini_ex7/sketch.js
@@ -0,0 +1,82 @@
+var old = 99;
+var verse = 0;
+var homex = 0;
+var homey = 0;
+var oldman = 0;
+
+function setup () {
+  createCanvas(5, 5);
+}
+
+function hePlays(number) {
+    verse = number
+  }
+
+function draw () {
+this.man = old;
+hePlays(1)
+knickknack('thumb','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(2);
+knickknack('shoe','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(3);
+knickknack('knee','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(4);
+knickknack('door','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(5);
+//knickknack('hive','that')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(6);
+knickknack('sticks','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(7);
+knickknack('heaven')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(8);
+knickknack('gate','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(9);
+knickknack('vine','my')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+
+this.man = old;
+hePlays(10);
+knickknack('repeat')
+knickknackpaddywack('bone','dog')
+oldman.position = homex, homey
+}
+
+function knickknackpaddywack (object,receiver) {
+  receiver = object
+}
+function knickknack (object, receiver) {
+  receiver = object
+}
